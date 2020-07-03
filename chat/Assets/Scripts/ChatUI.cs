8e32721630fcf587638c47401970ab4ba8f11060
Yuan Wentao <1099862512@qq.com>
2020-07-03 18:13:16 +0800

    初次提交

---
+++ chat/Assets/Scripts/ChatUI.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FairyGUI;
+using static FairyGUI.UIContentScaler;
+
+public class ChatUI : MonoBehaviour
+{
+    GComponent _mainView;
+    GList _list;
+    GTextInput _textInput;
+
+    List<string> contentList = new List<string>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Stage.inst.onKeyDown.Add(OnKeyDown);
+        _mainView = this.GetComponent<UIPanel>().ui;
+        _mainView.GetChild("send_btn").onClick.Add(OnClickSend);
+        
+        _list = _mainView.GetChild("list").asList;
+        _list.itemRenderer = RenderListItem;
+        _list.numItems = contentList.Count;
+
+        _textInput = _mainView.GetChild("input").asTextInput;
+    }
+
+    void OnClickSend()
+    {
+        contentList.Add(_textInput.text);
+        _list.numItems = contentList.Count;
+        _list.scrollPane.ScrollBottom();
+    }
+
+    void RenderListItem(int index, GObject obj)
+    {
+        GComponent component = obj.asCom;
+        component.GetChild("content").asTextField.text = contentList[index];
+    }
+
+    void OnKeyDown(EventContext context)
+    {
+        if (context.inputEvent.keyCode == KeyCode.Escape)
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ chat/Assets/Scripts/FguiLuaBehaviour.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+using System;
+using FairyGUI;
+
+public class FguiLuaBehaviour : MonoBehaviour
+{
+    public TextAsset luaScript;
+
+    internal static LuaEnv luaEnv = new LuaEnv(); //all lua behaviour shared one luaenv only!
+    internal static float lastGCTime = 0;
+    internal const float GCInterval = 1;//1 second 
+
+    private Action luaStart;
+    private Action luaUpdate;
+    private Action luaOnDestroy;
+
+    private LuaTable scriptEnv;
+
+    void Awake()
+    {
+        scriptEnv = luaEnv.NewTable();
+
+        // 为每个脚本设置一个独立的环境，可一定程度上防止脚本间全局变量、函数冲突
+        LuaTable meta = luaEnv.NewTable();
+        meta.Set("__index", luaEnv.Global);
+        scriptEnv.SetMetaTable(meta);
+        meta.Dispose();
+
+        scriptEnv.Set("self", this);
+
+        UIPanel panel = this.GetComponent<UIPanel>();
+        if (panel != null)
+        {
+            scriptEnv.Set("panel", panel);
+        }
+
+        luaEnv.DoString(Resources.Load<TextAsset>("Lua/FairyGUI.lua").text, "FairyGUI", scriptEnv);
+        luaEnv.DoString(luaScript.text, "LuaTestScript", scriptEnv);
+
+        Action luaAwake = scriptEnv.Get<Action>("awake");
+        scriptEnv.Get("start", out luaStart);
+        scriptEnv.Get("update", out luaUpdate);
+        scriptEnv.Get("ondestroy", out luaOnDestroy);
+
+        if (luaAwake != null)
+        {
+            luaAwake();
+        }
+
+        Stage.inst.onKeyDown.Add(OnKeyDown);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        if (luaStart != null)
+        {
+            luaStart();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (luaUpdate != null)
+        {
+            luaUpdate();
+        }
+        if (Time.time - FguiLuaBehaviour.lastGCTime > GCInterval)
+        {
+            luaEnv.Tick();
+            FguiLuaBehaviour.lastGCTime = Time.time;
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (luaOnDestroy != null)
+        {
+            luaOnDestroy();
+        }
+        luaOnDestroy = null;
+        luaUpdate = null;
+        luaStart = null;
+        scriptEnv.Dispose();
+    }
+
+    void OnKeyDown(EventContext context)
+    {
+        if (context.inputEvent.keyCode == KeyCode.Escape)
+        {
+            Application.Quit();
+        }
+    }
+}
